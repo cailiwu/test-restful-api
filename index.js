b6d133d191d6ff2a73c0652013d02caa36e05c5d
CaiLI <dy5025115@gmail.com>
2020-03-13 16:02:36 +0800

    Added post course api

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ app.delete('/api/courses/:id', (req, res) => {
   res.send(course);
 });
 
+// Post course
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
 function validateCourse (course) {
   const schema = {
     name: Joi.string().min(3).required()
