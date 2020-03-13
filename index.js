23e1953cc9aebbef49fa98ef5ecdf8143f54efa6
CaiLI <dy5025115@gmail.com>
2020-03-13 15:02:57 +0800

    Added eslint js standard style

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ const courses = [
 // GET all courses
 app.get('/api/course', (req, res) => {
   res.send(courses);
-})
+});
 
 app.get('/api/courses/:id', (req, res) => {
   console.log(typeof req.params.id);
   const course = courses.find((item) => item.id === parseInt(req.params.id));
   console.log(course);
-  if(!course) return res.status(404).send(`The course id ${req.params.id} is not found.`);
+  if (!course) return res.status(404).send(`The course id ${req.params.id} is not found.`);
   res.send(course);
-})
+});
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => {console.log(`start on port ${port}...`)});
+app.listen(3000, () => { console.log(`start on port ${port}...`); });
